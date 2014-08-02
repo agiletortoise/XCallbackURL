9e358c2c11c0b7aec3f2d418c074126dca477ac6
Ayaka Nonaka <ayanonagon@gmail.com>
2014-08-02 14:39:11 -0700

    [ReceiveViewController] Remove methods that just call super class implementation

--- Classes/ReceiveViewController.m
+++ Classes/ReceiveViewController.m
@@ -36,35 +36,4 @@
 	[urlTextView setText:[url absoluteString]];
 }
 
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-*/
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations.
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
-
-- (void)didReceiveMemoryWarning {
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc. that aren't in use.
-}
-
-- (void)viewDidUnload {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-
-
-
 @end
